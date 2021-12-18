2597c6ac4c280709d1e2e64e1c6b60c13b6488e7
baldr <kurnuli2010@yandex.ru>
2021-12-18 22:27:56 +0300

    first_comm

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,108 @@
 const content = document.querySelector('.content');
-const popup = document.querySelector('.popup');
+const popupEdit = document.querySelector('.popup_type_edit-profile');
+const popupAdd = document.querySelector('.popup_type_add-post');
+const popupImage = document.querySelector('.popup_type_image-view');
 const profileEditButton = content.querySelector('.profile__button-edit');
-const popupCloseButton = content.querySelector('.popup__close-button');
-
-const formElement = document.querySelector('.popup__form');
+const addPostButton = content.querySelector('.profile__add-post-button');
+const popupCloseButtonEdit = content.querySelector('.popup__close-button_type_edit');
+const popupCloseButtonAdd = content.querySelector('.popup__close-button_type_add');
+const popupCloseButtonImageView = content.querySelector('.popup__close-button_type_view');
+const formElementEdit = document.querySelector('.popup__form_type_edit');
+const formElementAdd = document.querySelector('.popup__form_type_add');
 const nameInput = document.querySelector('.popup__form-item_type_name');
 const prophecyInput = document.querySelector('.popup__form-item_type_prophecy');
+const postLinkInput = document.querySelector('.popup__form-item_type_link');
+const postTitleInput = document.querySelector('.popup__form-item_type_title');
 const profileUsername = content.querySelector('.profile__username');
 const profileProphecy = content.querySelector('.profile__prophecy');
+const postContainer = content.querySelector('.posts');
+const postImage = content.querySelector('.popup__image_view');
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 
 
 /**
- * Функция открытия поп-апа, для редактирования профиля
+ * Функция для работы с создание/удалением/изменений состояния поста.
+ * @param postLink
+ * @param postTitle
  */
-function openPopup() {
-    nameInput.value = profileUsername.textContent;
-    prophecyInput.value = profileProphecy.textContent;
-    popup.classList.add('popup_opened');
+function addPost(postLink, postTitle) {
+    const postTemplate = document.querySelector('#post-template').content;
+    const postElement =  postTemplate.querySelector('.post').cloneNode(true);
+
+    postElement.querySelector('.post__image').src = postLink;
+    postElement.querySelector('.post__title').textContent = postTitle;
+    postElement.querySelector('.post__like').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('post__like_active');
+    });
+    postElement.querySelector('.post__delete').addEventListener('click', function () {
+        postElement.remove()
+    });
+    postElement.querySelector('.post__image').addEventListener('click', function (evt) {
+        openPopup(evt, postLink)
+    });
+    postContainer.prepend(postElement);
 }
 
 /**
- * Функция закрытия поп-апа, для редактирования профиля
+ * Функция рендера постов
+ */
+function renderPosts(){
+    initialCards.forEach(function (item) {
+    addPost(item.link, item.name)
+    });
+}
+
+
+/**
+ * Функция открытия поп-апа
+ */
+function openPopup(evt, image_link='d') {
+    if (evt.target.className.includes('profile__button-edit')){
+        nameInput.value = profileUsername.textContent;
+        prophecyInput.value = profileProphecy.textContent;
+        popupEdit.classList.add('popup_opened');
+    } else if (evt.target.className.includes('profile__add-post-button')){
+        popupAdd.classList.add('popup_opened');
+    } else if(evt.target.className.includes('post__image')){
+        popupImage.classList.add('popup_opened');
+        postImage.src = image_link;
+    }
+}
+
+/**
+ * Функция закрытия поп-апа
  */
 function closePopup() {
-    popup.classList.remove('popup_opened');
+    popupEdit.classList.remove('popup_opened');
+    popupAdd.classList.remove('popup_opened');
+    popupImage.classList.remove('popup_opened');
 }
-
 
 /**
  * Обработчик кнопки принятия формы
@@ -33,12 +110,27 @@ function closePopup() {
  */
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    profileUsername.textContent = nameInput.value
-    profileProphecy.textContent = prophecyInput.value
-    closePopup()
+    if(evt.target.className.includes('popup__form_type_edit')){
+        profileUsername.textContent = nameInput.value;
+        profileProphecy.textContent = prophecyInput.value;
+        closePopup();
+    } else {
+        addPost(postLinkInput.value, postTitleInput.value);
+        postLinkInput.value = '' ;
+        postTitleInput.value = '' ;
+        closePopup();
+    }
+
 }
 
+renderPosts()
 profileEditButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
+addPostButton.addEventListener('click', openPopup);
 
-formElement.addEventListener('submit', formSubmitHandler);
+popupCloseButtonEdit.addEventListener('click', closePopup);
+popupCloseButtonAdd.addEventListener('click', closePopup);
+popupCloseButtonImageView.addEventListener('click', closePopup);
+
+formElementEdit.addEventListener('submit', formSubmitHandler);
+formElementAdd.addEventListener('submit', formSubmitHandler);
+
